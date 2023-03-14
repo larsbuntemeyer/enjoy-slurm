e468a784d815318c7e191ec39a8ab7d49fd9e284
Lars Buntemeyer <lars.buntemeyer@gmail.com>
2023-03-14 15:52:29 +0100

    added tests

---
+++ tests/test_parser.py
@@ -0,0 +1,13 @@
+import pytest
+
+from enjoy_slurm.utils import parse_dependency, kwargs_to_list
+
+
+def test_parse_dependency():
+    assert parse_dependency([1, 2, 3]) == "afterok:1:2:3"
+    assert parse_dependency([1, 2, 3], how="afterany") == "afterany:1:2:3"
+
+
+def test_kwargs_to_list():
+    kwargs = {"partition": "test", "dependency": [1, 2, 3]}
+    assert kwargs_to_list(kwargs) == ["--partition=test", "--dependency=afterok:1:2:3"]
