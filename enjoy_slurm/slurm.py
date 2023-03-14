a276c809c9cf1b4565aa3607dc8366eff492ee59
Lars Buntemeyer <lars.buntemeyer@gmail.com>
2023-03-14 16:51:12 +0100

    added docstrings

--- enjoy_slurm/slurm.py
+++ enjoy_slurm/slurm.py
@@ -5,6 +5,20 @@ from .utils import kwargs_to_list, parse_sacct, execute
 
 
 def sbatch(jobscript, *args, **kwargs):
+    """
+    Submit a batch script to Slurm
+
+    Parameters
+    ----------
+    jobscript : str
+        Path to jobscript file.
+
+    Returns
+    -------
+    jobid: int
+        Slurm jobid.
+
+    """
     logging.info("submitting: " + jobscript)
     command = (
         ["sbatch", "--parsable"] + list(args) + kwargs_to_list(kwargs) + [jobscript]
@@ -15,9 +29,24 @@ def sbatch(jobscript, *args, **kwargs):
 
 
 def sacct(jobid=None, format=None, **kwargs):
+    """
+    Accounting data for all jobs and job steps in the Slurm job accounting log or Slurm database
+
+    Parameters
+    ----------
+    jobid : int
+        If provided, displays information about the specified job.
+    format: list
+        List of columns that can be specified.
+
+    Returns
+    -------
+    sacct info: DataFrame
+        Slurm accounting data.
+
+    """
     if format is None and not kwargs:
         format = ["jobid", "elapsed", "ncpus", "ntasks", "state", "end", "jobname"]
-    print(format)
     command = ["sacct", "--parsable2"] + kwargs_to_list(kwargs)
     if format:
         command += ["--format=" + ",".join(format)]
@@ -25,13 +54,3 @@ def sacct(jobid=None, format=None, **kwargs):
         command += ["-j", str(jobid)]
     output = execute(command)
     return parse_sacct(output)
-
-
-class Scheduler:
-    def __init__(self):
-        pass
-
-
-class Job:
-    def __init__(self):
-        pass
