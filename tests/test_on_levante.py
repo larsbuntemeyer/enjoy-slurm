9974299fd543d658e774dfdd69a9140efc66b11f
Lars Buntemeyer <lars.buntemeyer@gmail.com>
2023-03-19 22:44:11 +0100

    updates

--- tests/test_on_levante.py
+++ tests/test_on_levante.py
@@ -1,16 +1,28 @@
 import pytest
 import enjoy_slurm as slurm
-from . import requires_levante
+from . import hostname, requires_slurm, on_levante, requires_levante
 
 
-@requires_levante
+test_kwargs = {
+    "lvt.dkrz.de": {"partitions": ["compute", "shared"]},
+    "docker": {"partitions": ["debug", "normal"]},
+}
+
+print(hostname)
+
+
+@requires_slurm
 def test_sbatch():
-    jobid = slurm.sbatch(wrap="echo Hello World", partition="shared", account="ch0636")
+    if on_levante:
+        kwargs = {"partition": "shared", "account": "ch0636"}
+    else:
+        kargs = {}
+    jobid = slurm.sbatch(wrap="echo Hello World", **kwargs)
     print(jobid)
 
 
-@pytest.mark.parametrize("partition", ["compute", "shared"])
-@requires_levante
+@pytest.mark.parametrize("partition", test_kwargs[hostname]["partitions"])
+@requires_slurm
 def test_partitions(partition):
     partitions = slurm.scontrol.show(partition=partition)
     assert partition in partitions
