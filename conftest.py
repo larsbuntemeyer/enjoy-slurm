c1b64450f432fb44c5112282d63febabedc925ea
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-03-21 11:36:58 +0100

    fix

--- conftest.py
+++ conftest.py
@@ -46,17 +46,3 @@ def pytest_runtest_setup(item):
         if env in xfail:
             item.add_marker(pytest.mark.xfail(reason=xfail[env]))
 
-
-@pytest.fixture(autouse=True)
-def mock_lsf_version(monkeypatch, request):
-    # Monkey-patch lsf_version() UNLESS the 'lsf' environment is selected.
-    # In that case, the real lsf_version() function should work.
-    markers = list(request.node.iter_markers())
-    if any("lsf" in marker.args for marker in markers):
-        return
-
-    try:
-        dask_jobqueue.lsf.lsf_version()
-    except OSError:
-        # Provide a fake implementation of lsf_version()
-        monkeypatch.setattr(dask_jobqueue.lsf, "lsf_version", lambda: "10")
